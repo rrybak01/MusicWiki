c5b741f102bf7509c28df6f59a68aed09e79cb71
rrybak01 <rrybak01@educantabria.es>
2022-04-13 21:47:36 +0200

    Dejar elemento seleccionado

--- src/Components/Estructura/Sidebar.jsx
+++ src/Components/Estructura/Sidebar.jsx
@@ -5,36 +5,36 @@ import { Link } from 'react-router-dom';
 import uuid from "react-uuid";
 
 class Sidebar extends React.Component {
-
   render(){
-    return(
-      <div className="Sidebar">
-        <ul className='lista'>
-          <div className="Sidebar-header">
-            MusicWiki
-          </div>
-          <div>
-            {SidebarData.map((item) => {
-              return (
-                <li 
-                  key={uuid()} 
-                  className="elementosLista"
-                >
-                  <Link
-                    to={item.link}
-                    className="sidebar-link"> 
-                      <div id="icono">{item.icon}</div>
-                      <div id="titulo">{item.title}</div>
-                  </Link>
+      return (
+          <div className="Sidebar">
+              <ul className="lista">
+                  <div className="Sidebar-header">
+                      MusicWiki
+                  </div>
+                  <div>
+                  {SidebarData.map((item) => {
+                  return(
+                      <li key={uuid()}
+                      className="elementosLista"
+                      id={window.location.pathname == item.link ? "active" : ""}
+                      onClick={()=>{window.location.pathname = item.link}}>
+                          <Link
+                              to={item.link}
+                              className="sidebar-link"> 
+                              <div id="icono">{item.icon}</div>
+                              <div id="titulo">{item.title}</div>
+                          </Link>
+                      </li>
+                  )
+                  })}
+                  </div>
                   
-                </li>
-              );
-            })}
+              </ul>
           </div>
-        </ul>
-      </div>
-    )
-  } 
+        );
+  }
+
 }
 
 export default Sidebar;
